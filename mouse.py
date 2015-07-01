f6ca2c243fb295f367bb4de05edaa13bf823eec9
YLGH <YingLiu@dhcp-172-17-25-219.wrd.wireless.private.cam.ac.uk>
2015-07-01 10:41:07 +0100

    mouse

--- mouse.py
+++ mouse.py
@@ -1,24 +1,6 @@
 from Xlib import X, display
-<<<<<<< HEAD
-import time
-
-d = display.Display()
-s = d.screen()
-root = s.root
-root.warp_pointer(500,500)
-d.sync()
-
-time.sleep( 5 )
-root.warp_pointer(250,250)
-d.sync()
-
-time.sleep( 5 )
-root.warp_pointer(100,100)
-d.sync()
-
-time.sleep( 5 )
-root.warp_pointer(250,250)
-d.sync()
-=======
-import time
->>>>>>> 204a63b0984787efd908eca157ac25de7bedbeab
+d = display.Display();
+s = d.screen();
+root = s.root;
+root.wrap_pointer(300,300);
+d.sync();
