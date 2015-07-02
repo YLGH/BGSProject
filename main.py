de68f27b6ce33949e3f3b28b2c1a148e198a3711
Ying <Ying>
2015-07-02 14:33:59 +0000

    test

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from generateData import generateData;
 from GaussNewtonMultiLat import getLocation
 import time
 
-for i in range(5):
+for i in range(100):
 	generateData();
 
 	p = getLocation();
 	x = p[0];
 	y = p[1];
 	moveTo(x,y);
-	time.sleep(1);
+	time.sleep(.5);
