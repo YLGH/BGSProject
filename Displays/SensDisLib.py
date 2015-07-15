f71de065a07024de7d565757b7562e46c6d46cd9
Ying <Ying>
2015-07-15 16:14:54 +0000

    LIBRARY BETA WORKS

--- Displays/SensDisLib.py
+++ Displays/SensDisLib.py
@@ -11,119 +11,117 @@ class SensorDisplay:
 
 	def __init__(self, displayTime = 2):
 		#input is the maxDisplayTime in seconds
-		numSensors = 0
+		self.numSensors = 0
 
-		spi = spidev.SpiDev()
-		spi.open(0,0)
+		self.spi = spidev.SpiDev()
+		self.spi.open(0,0)
 
-		maxlength = displayTime * 1000
-		data = [deque(maxlen=maxlenx),deque(maxlen=maxlenx),deque(maxlen=maxlenx),deque(maxlen=maxlenx)]
-		qx = deque(maxlen=maxlenx)
+		maxlenx = displayTime * 1000
+		self.data = [deque(maxlen=maxlenx),deque(maxlen=maxlenx),deque(maxlen=maxlenx),deque(maxlen=maxlenx)]
+		self.qx = deque(maxlen=maxlenx)
 
-		app = QtGui.QApplication([])
-		win = pg.GraphicsWindow(title = "Sensor Displays")
-		win.resize(1000, 800)
-		win.setWindowTitle('Sensor Displays')
+		self.app = QtGui.QApplication([])
+		self.win = pg.GraphicsWindow(title = "Sensor Displays")
+		self.win.resize(1000, 800)
+		self.win.setWindowTitle('Sensor Displays')
 
-		sensorSet = [False, False, False, False]
+		self.sensorSet = [False, False, False, False]
 
 		pg.setConfigOptions(antialias = False)
 
 	#Add Sensor One
 
 	def add_Sensor_One(self, graphTitle = "Sensor One"):
-		if(not sensorSet[0]):
-			if numSensors == 2:
+		if(not self.sensorSet[0]):
+			if self.numSensors == 2:
 				win.nextRow()
 
-			p1.win.addPlot(title = graphTitle)
-			p1.setYRange(0, 4096) #default Y range
-			curve1 = p1.plot(pen = 'r')
-			numSensors += 1
+			self.p1 = self.win.addPlot(title = graphTitle)
+			self.p1.setYRange(0, 4096) #default Y range
+			self.curve1 = self.p1.plot(pen = 'r')
+			self.numSensors += 1
 
-			sensorSet[0] = True
+			self.sensorSet[0] = True
 
 	def add_Sensor_Two(self, graphTitle = "Sensor Two"):
-		if(not sensorSet[1]):
-			if numSensors == 2:
-				win.nextRow()
+		if(not self.sensorSet[1]):
+			if self.numSensors == 2:
+				self.win.nextRow()
 
-			p2.win.addPlot(title = graphTitle)
-			p2.setYRange(0, 4096)
-			curve2 = p2.plot(pen = 'b')
-			numSensors += 1
+			self.p2 = self.win.addPlot(title = graphTitle)
+			self.p2.setYRange(0, 4096)
+			self.curve2 = self.p2.plot(pen = 'g')
+			self.numSensors += 1
 
-			sensorSet[1] = True
+			self.sensorSet[1] = True
 
 	def add_Sensor_Three(self, graphTitle = "Sensor Three"):
-		if(not sensorSet[2]):
-			if numSensors == 2:
-				win.nextRow()
+		if(not self.sensorSet[2]):
+			if self.numSensors == 2:
+				self.win.nextRow()
 
-			p3.win.addPlot(title = graphTitle)
-			p3.setYRange(0, 4096)
-			curve3 = p3.plot(pen = 'g')
-			numSensors += 1
+			self.p3 = self.win.addPlot(title = graphTitle)
+			self.p3.setYRange(0, 4096)
+			self.curve3 = self.p3.plot(pen = 'b')
+			self.numSensors += 1
 
-			sensorSet[2] = True
+			self.sensorSet[2] = True
 
 	def add_Sensor_Four(self, graphTitle = "Sensor Four"):
-		if(not sensorSet[3]):
-			if numSensors == 2:
-				win.nextRow()
+		if(not self.sensorSet[3]):
+			if self.numSensors == 2:
+				self.win.nextRow()
 
-			p4.win.addPlot(title = graphTitle)
-			p4.setYRange(0, 4096)
-			curve4 = p4.plot(pen = 'y')
-			numSensors += 1
+			self.p4 = self.win.addPlot(title = graphTitle)
+			self.p4.setYRange(0, 4096)
+			self.curve4 = self.p4.plot(pen = 'y')
+			self.numSensors += 1
 
-			sensorSet[3] = True
+			self.sensorSet[3] = True
 
 	def setYRange_Sensor_One(self, low, high):
-		p1.setYRange(low, high);
+		self.p1.setYRange(low, high);
 
 	def setYRange_Sensor_Two(self, low, high):
-		p2.setYRange(low, high);
+		self.p2.setYRange(low, high);
 
 	def setYRange_Sensor_Three(self, low, high):
-		p3.setYRange(low, high);
+		self.p3.setYRange(low, high);
 
 	def setYRange_Sensor_Four(self, low, high):
-		p4.setYRange(low, high);
+		self.p4.setYRange(low, high);
 
-	def update():
-		global curve1, curve2, curve3, curve4, ptr, p1, p2, p3, p4, qx, data
-		if(sensorSet[0]):
-			curve1.setData(qx, data[0])
-		if(sensorSet[1]):
-			curve2.setData(qx, data[1])
-		if(sensorSet[2]):
-			curve3.setData(qx, data[2])
-		if(sensorSet[3]):
-			curve4.setData(qx, data[3])
+	def update(self):
+		if(self.sensorSet[0]):
+			self.curve1.setData(self.qx,self.data[0])
+		if(self.sensorSet[1]):
+			self.curve2.setData(self.qx,self.data[1])
+		if(self.sensorSet[2]):
+			self.curve3.setData(self.qx, self.data[2])
+		if(self.sensorSet[3]):
+			self.curve4.setData(self.qx,self.data[3])
 
-	def getNext():
-		global ptr
+	def getNext(self):
 		ptr = 0
 
 		while True:
 			time.sleep(.0001)
-			byteArray = spi.xfer([0x01])	
-			byteArray = spi.xfer([0xff]*8)
+			byteArray = self.spi.xfer([0x01])	
+			byteArray = self.spi.xfer([0xff]*8)
 			for i in range(0, 4):
 				toAdd = (byteArray[(2*i)] << 8) + byteArray[(2*i)+1]
-				data[i].append(toAdd)
-				qx.append(ptr)
+				self.data[i].append(toAdd)
+			self.qx.append(ptr)
 			ptr+=1
 
-	def run():
-		input_Thread = threading.Thread(target = getNext)
-		input_Thread.daemon = True
-		input_Thread.start()
+	def run(self):
+		self.input_Thread = threading.Thread(target = self.getNext)
+		self.input_Thread.daemon = True
+		self.input_Thread.start()
 
-		timer = QtCore.QTimer()
-		timer.timeout.connect(update)
-		timer.start()
+		self.timer = QtCore.QTimer()
+		self.timer.timeout.connect(self.update)
+		self.timer.start()
 
 		import sys
 		if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
---
+++ Displays/test.py
@@ -0,0 +1,9 @@
+import SensDisLib as s
+
+test = s.SensorDisplay()
+test.add_Sensor_One("Geophone One")
+test.add_Sensor_Two("Geophone Two")
+test.add_Sensor_Three("TWANGER")
+test.add_Sensor_Four("Temperature")
+test.setYRange_Sensor_Four(1100, 1600)
+test.run()
