94f1cc910df9ec5e457c123ec3e65a34959cf73a
YLGH <YingLiu@dhcp-172-17-25-219.wrd.wireless.private.cam.ac.uk>
2015-07-15 10:43:35 +0100

    four display

--- SensorDisplay.py
+++ SensorDisplay.py
@@ -41,13 +41,13 @@ p1 = win.addPlot(title="Sensor One")
 curve1=p1.plot(pen='r')
 def update1():
     global curve1, p1
-    curve1.setData(px, data[0])
+    curve1.setData(qx, data[0])
 
 p2 = win.addPlot(title="Sensor Two")
 curve2 = p2.plot(pen = 'g')
 def update2():
     global curve2, p2
-    curve2.setData(px, data[1])
+    curve2.setData(qx, data[1])
 
 win.nextRow()
 
@@ -55,17 +55,17 @@ p3 = win.addPlot(title="Sensor Three")
 curve3 = p3.plot(pen = 'b')
 def update3():
     global curve3, p3
-    curve3.setData(px, data[2])
+    curve3.setData(qx, data[2])
 
 p4 = win.addPlot(title="Sensor Four")
 curve4 = p4.plot(pen='y')
 ptr = 0
 def update4():
     global curve4, data, ptr, p4
-    curve4.setData(px, data[3])
+    curve4.setData(qx, data[3])
 
 def update():
-    global curve1, curve2, curve3, curve4, ptr, p1, p2, p3, p4
+    global curve1, curve2, curve3, curve4, ptr, p1, p2, p3, p4, qx
     update1()
     update2()
     update3()
