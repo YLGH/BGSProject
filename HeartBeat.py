337ecd76f0346236f860e2abb4d982445eef3578
YLGH <YingLiu@Bronze5.local>
2015-07-17 13:29:57 +0100

    Heart

--- HeartBeat.py
+++ HeartBeat.py
@@ -24,4 +24,31 @@ def addValue(i):
 sensor.add_sensor_one("Heartbeat")
 sensor.setVoltageFunction_sensor(1, addValue)
 
-sensor.runPlot()
+numPeaks = 0
+lastTime = 0
+maxIteration = 20
+threshold = 2040
+timeDifferences = []
+
+lastVoltage = 0
+thisVoltage = 0
+while(numPeaks < maxIteration):
+	lastVoltage = thisVoltage
+	thisVoltage = sensor.get_raw_sensor(1);
+	if(lastVoltage < threshold and thisVoltage > threshold):
+		if(numPeaks > 0):
+			timeDifferences.append(time.time() - lastTime)
+			lastTime = time.time()
+			numPeaks+=1
+		else:
+			lastTime = time.time()
+			numPeaks+=1
+
+totalSum = 0.0
+for i timeDifferences:
+	totalSum += i
+
+totalSum = totalSum/len(timeDifferences)
+
+print totalSum;
+
