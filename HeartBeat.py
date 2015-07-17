0f72ec74122317f00a8298c1606572c5b46af488
YLGH <YingLiu@Bronze5.local>
2015-07-17 12:19:26 +0100

    moving point

--- HeartBeat.py
+++ HeartBeat.py
@@ -1,38 +1,27 @@
-import Displays/SensDisLib as s
+import SensDisLib as s
 import time as t
+from collections import deque
 
 sensor = s.SensorDisplay()
 
 #We want to see a total of 17 peaks, for 16 time differences
 
-numPeaks = 0
-threshHold = 2025
 
-peakTimes = list()
+buff16 = deque(maxlen = 16)
+currentSum = 0.0
 
-lastVoltage = 0
-thisVoltage = 0
-while numPeaks < 17:
-	lastVoltage = thisVoltage
-	thisVoltage = 0
-	#average over 16 measurements
-	for i in xrange(64):
-		thisVoltage += sensor.get_Raw_Sensor(1)
-	thisVoltage /= 64
-	if(thisVoltage >= threshHold and lastVoltage < threshHold):
-		peakTimes.append(t.time())
-		numPeaks += 1
-	lastVoltage = thisVoltage
+def addValue(i):
+	global currentSum
+	if(len(buff16) == 16):
+		currentSum -= buff16[0]
 
-timeDifference = []
-
-for i in range(1, 16):
-	timeDifference.append(peakTimes[i]-peakTimes[i-1])
-
-print peakTimes
-print timeDifference
+	buff16.append(i)
+	currentSum += i;
+	return currentSum/len(buff16)
 
 
+	
+sensor.add_sensor_one("Heartbeat")
+sensor.setVoltageFunction_sensor(1, addValue)
 
-
-
+sensor.runPlot()
