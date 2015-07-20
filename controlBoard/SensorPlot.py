b00344c1f65b6ed0a8dd8c6068fad156aeaa87ca
Ying <Ying>
2015-07-20 13:02:46 +0000

    fixes

--- controlBoard/SensorPlot.py
+++ controlBoard/SensorPlot.py
@@ -25,9 +25,11 @@ class SensorPlot:
 		self.win.resize(1000, 800)
 		self.win.setWindowTitle('Sensor Plots')
 
-		self.sensors = [Sensor(), Sensor(), Sensor(), Sensor()]
+		self.sensors = [Sensor.Sensor(), Sensor.Sensor(), Sensor.Sensor(), Sensor.Sensor()]
 		self.plot = [None, None, None, None]
 		self.curve = [None, None, None, None]
+
+		self.colors = ['r','g','b','y']
 
 
 		pg.setConfigOptions(antialias = False)
@@ -35,18 +37,16 @@ class SensorPlot:
 	
 	def add_sensor(self, index, graphTitle = "New Sensor"):
 		index -= 1;
-		assert(not sensors[index].toLog), "This sensor is already added!"
+		assert(not self.sensors[index].toLog), "This sensor is already added!"
 
 		if(self.numSensors == 2):
 			self.win.nextRow()
+		self.sensors[index].toLog = True
 
 		self.plot[index] = self.win.addPlot(title = graphTitle)
 		self.plot[index].setYRange(0, 4096)
-
-
-		self.p1 = self.win.addPlot(title = graphTitle)
-		self.p1.setYRange(0, 4096) #default Y range
-		self.curve[index] = self.p1.plot(pen = 'r')
+	
+		self.curve[index] = self.plot[index].plot(pen = self.colors[index])
 
 		self.numSensors += 1
 
@@ -56,8 +56,8 @@ class SensorPlot:
 
 	def update(self):
 		for i in range(0, 4):
-			if(self.sensor[0].toLog):
-				self.curve[index].setData(self.qx, self.data[0])
+			if(self.sensors[i].toLog):
+				self.curve[i].setData(self.qx, self.data[i])
 
 	def getNext(self):
 		ptr = 0
@@ -66,9 +66,9 @@ class SensorPlot:
 			byteArray = self.spi.xfer([0x01])#sending a "I need data bit"
 			byteArray = self.spi.xfer([0xff]*8) #retrieving the 8 bits
 			for i in range(0, 4):
-				if(self.sensor[i].toLog):
+				if(self.sensors[i].toLog):
 					toAdd = (byteArray[(2*i)] << 8) + byteArray[(2*i)+1]
-					self.data[i].append(self.voltageFunction[i](toAdd))
+					self.data[i].append(toAdd)
 			self.qx.append(ptr)
 			ptr+=1
 
---
+++ controlBoard/test.py
@@ -0,0 +1,11 @@
+import SensorPlot as s
+
+test = s.SensorPlot()
+
+test.add_sensor(1, "ONE")
+test.setYRange_sensor(1, 1500, 2500)
+#test.add_sensor(2, "TWO")
+#test.add_sensor(3, "THREE")
+#test.add_sensor(4, "FOUR")
+
+test.runPlot()
