92ede7a4123aa2f474b6042cb831db29b0841489
Ying <Ying>
2015-07-22 13:21:13 +0000

    latest

--- controlBoard/boardControlLib.py
+++ controlBoard/boardControlLib.py
@@ -18,10 +18,12 @@ class BoardControlLib:
 		self.spi.open(0,0)
 
 	def set_CSV(self):
+		assert(not self.recording), "Can't change while recording!"
 		fileType = f.CSV(self.spi)
 		fileType.indicate()
 
 	def set_Binary(self):
+		assert(not self.recording), "Can't change while recording!"
 		fileType = f.Binary(self.spi)
 		fileType.indicate()
 
@@ -33,6 +35,7 @@ class BoardControlLib:
 		else: return "ERROR"
 
 	def set_sample_rate(self, sampleRate):
+		assert(not self.recording), "Can't change while recording!"
 		self.spi.xfer([0x03])
 		self.spi.xfer([2])
 		sd = int(1000/sampleRate)
@@ -55,6 +58,7 @@ class BoardControlLib:
 		self.recording = False
 
 	def initialize_card(self):
+		assert(not self.recording), "Can't initialize while recording!"
 		self.spi.xfer([0x06])
 		reps = 0
 		while reps<20:
@@ -70,7 +74,8 @@ class BoardControlLib:
 		self.spi.xfer([0x07])
 		return (self.spi.xfer([0xff])[0] == 1)
 
-	def set_name_sensor(self, index, name_String):
+	def set_sensor_name(self, index, name_String):
+		assert(not self.recording), "Can't change while recording!"
 		self.spi.xfer([0x02])
 		self.spi.xfer([len(name_String) + 1])
 		self.spi.xfer([index-1])
@@ -78,13 +83,32 @@ class BoardControlLib:
 			self.spi.xfer([ord(letter)])
 		time.sleep(0.2)
 
-	def get_name_sensor(self, index):
+	def get_sensor_name(self, index):
 		self.spi.xfer([0x12])
 		self.spi.xfer([0x01])
 		self.spi.xfer([index-1])
 		namelen = self.spi.xfer([0xff])[0]
 		assert(namelen<32), "NAME TOO LONG?!?"
 		return ''.join(map(chr, self.spi.xfer([0xff]*namelen)))
+
+	def enable_sensor(self, index):
+		assert(not self.recording), "Can't change while recording!"
+		self.spi.xfer([0x15])
+		self.spi.xfer([0x01])
+		self.spi.xfer([index-1])
+		time.sleep(0.2)
+
+	def disable_sensor(self, index):
+		assert(not self.recording), "Can't change while recording!"
+		self.spi.xfer([0x16])
+		self.spi.xfer([0x01])
+		self.spi.xfer([index-1])
+		time.sleep(0.2)
+
+	def is_sensor_enabled(self, index):
+		self.spi.xfer([0x17])
+		val = self.spi.xfer([0xff])[0]
+		return (val & (1 << (index-1))) > 0
 
 	def get_firmware_string(self):
 		self.spi.xfer([0x11])
--- controlBoard/testq.py
+++ controlBoard/testq.py
@@ -5,47 +5,49 @@ test = b.BoardControlLib()
 
 print "Board version:", test.get_firmware_string()
 
-#print "Setting names..."
-#test.set_name_sensor(1, "Test 1")
-#test.set_name_sensor(2, "Test 2")
-#test.set_name_sensor(3, "Test 3")
-#test.set_name_sensor(4, "Test 4")
+print "Setting names..."
+test.set_sensor_name(1, "My Sensor 1")
+#test.set_sensor_name(2, "Test 2")
+#test.set_sensor_name(3, "Test 3")
+#test.set_sensor_name(4, "Test 4")
+
+print "Enabling sensor 1 only."
+test.enable_sensor(1)
+test.disable_sensor(2)
+test.disable_sensor(3)
+test.disable_sensor(4)
 
 print
 
-print "Sensor 1 name:", test.get_name_sensor(1)
-print "Sensor 2 name:", test.get_name_sensor(2)
-print "Sensor 3 name:", test.get_name_sensor(3)
-print "Sensor 4 name:", test.get_name_sensor(4)
-
-print
+print "Sensor 1 name:", test.get_sensor_name(1)
+print "Sensor 2 name:", test.get_sensor_name(2)
+print "Sensor 3 name:", test.get_sensor_name(3)
+print "Sensor 4 name:", test.get_sensor_name(4)
 
 print "Sample rate:", test.get_sample_rate()
 
-print
-
 print "File type:", test.get_file_format()
 
-#print "Setting sensor names..."
-#test.set_name_sensor(1, "Jamie's Heartbeat");
-#test.set_name_sensor(2, "The second sensor");
-#test.set_name_sensor(3, "The TWANGER");
-#test.set_name_sensor(4, "Temperature");
+print "Sensor 1 enabled:", test.is_sensor_enabled(1)
+print "Sensor 2 enabled:", test.is_sensor_enabled(2)
+print "Sensor 3 enabled:", test.is_sensor_enabled(3)
+print "Sensor 4 enabled:", test.is_sensor_enabled(4)
 
-#print "Connecting to card..."
-#if test.initialize_card():
-#	print "Card ready"
+
+
+print "Connecting to card..."
+if test.initialize_card():
+	print "Card ready"
 	
-#	print "Switching to CSV Format..."
+	#print "Switching to CSV Format..."
 	
 	#print "Logging .csv for 10 second at 0.5SPS..."
 	#test.set_CSV()
 	#test.set_sample_rate(.5)
-	#test.start_logging()
-	#time.sleep(10)
-	#test.stop_logging()
-	#print "Done!"
-	
+	test.start_logging()
+	time.sleep(2)
+	test.stop_logging()
+	print "Done!"
 	
 #	print "Logging .bin for 4 seconds at 1000SPS..."
 #	test.set_Binary()
@@ -54,5 +56,5 @@ print "File type:", test.get_file_format()
 #	time.sleep(4)
 #	test.stop_logging()
 #	print "Done!"
-#else:
-#	print "Card init failed!"
+else:
+	print "Card init failed!"
