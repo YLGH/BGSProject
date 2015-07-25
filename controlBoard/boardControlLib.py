9052cdc9f64439a58ca6eb60f11a73a8eacaf8eb
Jamie Wood <jamie.wood70@ntlworld.com>
2015-07-26 00:01:43 +0100

    updated boardcontrollib to reflect additions to protocol

--- controlBoard/boardControlLib.py
+++ controlBoard/boardControlLib.py
@@ -17,23 +17,15 @@ class BoardControlLib:
 		self.spi = spidev.SpiDev()
 		self.spi.open(0,0)
 
-	def set_CSV(self):
+	def set_sensor_name(self, index, name_String):
 		assert(not self.recording), "Can't change while recording!"
-		fileType = f.CSV(self.spi)
-		fileType.indicate()
-
-	def set_Binary(self):
-		assert(not self.recording), "Can't change while recording!"
-		fileType = f.Binary(self.spi)
-		fileType.indicate()
-
-	def get_file_format(self):
-		self.spi.xfer([0x14])
-		val = self.spi.xfer([0xff])[0]
-		if val == 1: return "Raw"
-		elif val == 2: return "CSV"
-		else: return "ERROR"
-
+		self.spi.xfer([0x02])
+		self.spi.xfer([len(name_String) + 1])
+		self.spi.xfer([index-1])
+		for letter in name_String:
+			self.spi.xfer([ord(letter)])
+		time.sleep(0.2)
+	
 	def set_sample_rate(self, sampleRate):
 		assert(not self.recording), "Can't change while recording!"
 		self.spi.xfer([0x03])
@@ -41,12 +33,6 @@ class BoardControlLib:
 		sd = int(100000/sampleRate)
 		self.spi.xfer([(sd >> 24) & 0xFF, (sd>>16) & 0xFF, (sd >> 8) & 0xFF, sd & 0xFF])
 		time.sleep(0.2)
-
-	def get_sample_rate(self):
-		self.spi.xfer([0x13])
-		val = self.spi.xfer([0xff]*4)
-		sr = 100000.0/((val[0]<<24) + (val[1]<<16) + (val[2]<<8) + val[3])
-		return sr
 
 	def start_logging(self):
 		assert(self.cardInitialized), "CARD IS NOT INITIALIZED"
@@ -75,15 +61,22 @@ class BoardControlLib:
 		self.spi.xfer([0x07])
 		return (self.spi.xfer([0xff])[0] == 1)
 
-	def set_sensor_name(self, index, name_String):
+	def set_Raw(self):
 		assert(not self.recording), "Can't change while recording!"
-		self.spi.xfer([0x02])
-		self.spi.xfer([len(name_String) + 1])
-		self.spi.xfer([index-1])
-		for letter in name_String:
-			self.spi.xfer([ord(letter)])
-		time.sleep(0.2)
-
+		fileType = f.Binary(self.spi)
+		fileType.indicate()
+		
+	def set_CSV(self):
+		assert(not self.recording), "Can't change while recording!"
+		fileType = f.CSV(self.spi)
+		fileType.indicate()
+		
+	def get_firmware_string(self):
+		self.spi.xfer([0x11])
+		namelen = self.spi.xfer([0xff])[0]
+		assert(namelen<32), "NAME TOO LONG?!?"
+		return ''.join(map(chr, self.spi.xfer([0xff]*namelen)))
+		
 	def get_sensor_name(self, index):
 		self.spi.xfer([0x12])
 		self.spi.xfer([0x01])
@@ -92,6 +85,19 @@ class BoardControlLib:
 		assert(namelen<32), "NAME TOO LONG?!?"
 		return ''.join(map(chr, self.spi.xfer([0xff]*namelen)))
 
+	def get_sample_rate(self):
+		self.spi.xfer([0x13])
+		val = self.spi.xfer([0xff]*4)
+		sr = 100000.0/((val[0]<<24) + (val[1]<<16) + (val[2]<<8) + val[3])
+		return sr
+		
+	def get_file_format(self):
+		self.spi.xfer([0x14])
+		val = self.spi.xfer([0xff])[0]
+		if val == 1: return "Raw"
+		elif val == 2: return "CSV"
+		else: return "ERROR"
+	
 	def enable_sensor(self, index):
 		assert(not self.recording), "Can't change while recording!"
 		self.spi.xfer([0x15])
@@ -111,35 +117,55 @@ class BoardControlLib:
 		val = self.spi.xfer([0xff])[0]
 		return (val & (1 << (index-1))) > 0
 
-	def get_firmware_string(self):
-		self.spi.xfer([0x11])
-		namelen = self.spi.xfer([0xff])[0]
-		assert(namelen<32), "NAME TOO LONG?!?"
-		return ''.join(map(chr, self.spi.xfer([0xff]*namelen)))
+	def set_scheduled_start(self, timestamp):
+		self.spi.xfer([0x18])
+		self.spi.xfer([4])
+		self.spi.xfer([(timestamp >> 24) & 0xFF, (timestamp>>16) & 0xFF, (timestamp >> 8) & 0xFF, timestamp & 0xFF])
+	
+	def set_scheduled_end(self, timestamp):
+		self.spi.xfer([0x19])
+		self.spi.xfer([4])
+		self.spi.xfer([(timestamp >> 24) & 0xFF, (timestamp>>16) & 0xFF, (timestamp >> 8) & 0xFF, timestamp & 0xFF])
+	
+	def enable_scheduling(self):
+		self.spi.xfer([0x20])
+		
+	def disable_scheduling(self):
+		self.spi.xfer([0x21])
+	
+	def set_rtc_time(self, timestamp):
+		self.spi.xfer([0x22])
+		self.spi.xfer([4])
+		self.spi.xfer([(timestamp >> 24) & 0xFF, (timestamp>>16) & 0xFF, (timestamp >> 8) & 0xFF, timestamp & 0xFF])
 
-
-
-
+	def save_settings(self):
+		self.spi.xfer([0x23])
 
 
 '''
 0x01: We want to retrieve the information on the four sensors
 0x02: We want to set the name - followed by which sensor - followed by a String (length, then characters)
-0x03: Set sample delay - followed by an integer
+0x03: Set sample delay - followed by a 4-byte integer (x10us)
 0x04: start logging
 0x05: stop logging
 0x06: initialize the card
 0x07: is card ready?
-0x08: Binary Filetype
+0x08: Raw Filetype
 0x09: CSV FileType
 0x0A-F: reserved for file type
 0x11: report firmware version String
 0x12: Get sensor name - followed by which sensor
-0x13: get filetype: 1 for Binary, 2 for CSV
-0x14: Get sample delay
+0x13: Get sample delay
+0x14: get filetype: 1 for Raw, 2 for CSV
 0x15: Enable sensor - followed by which sensor
 0x16: Disable sensor - followed by which sensor
 0x17: Get enabled sensors mask
+0x18: Set scheduled start time - followed by 4-byte unix timestamp
+0x19: Set scheduled end time - followed by 4-byte unix timestamp
+0x20: Enable scheduling
+0x21: Disable scheduling
+0x22: Set RTC time - followed by 4-byte unix timestamp
+0x23: Save settings to EEPROM
 '''
 
 
